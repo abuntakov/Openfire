5d335fd2470aa333059c7a0d27721bac788a4fe7
Alexander Buntakov <abuntakov@gmail.com>
2016-04-21 20:38:23 +0300

    change response message

--- src/plugins/spiritGuide/src/java/org/jivesoftware/openfire/plugin/spiritguide/SchedulerBot.java
+++ src/plugins/spiritGuide/src/java/org/jivesoftware/openfire/plugin/spiritguide/SchedulerBot.java
@@ -5,7 +5,7 @@ public class SchedulerBot implements Bot {
 
     public BotMessage getResponseMessage(String message) {
         if(message.contains("schedule")) {
-            return new BotMessage("Lunch at 7 o'clock", "scheduler_bot");
+            return new BotMessage("#schedule", "scheduler_bot");
         } else {
             return null;
         }
